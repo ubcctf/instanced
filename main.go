176941eed9c0e312e34bacef0093050f101e0e05
Jason Hsu <jasonhsu2013@gmail.com>
2023-08-23 22:40:16 -0700

    prototype non-scuffed client

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/labstack/echo/v4"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/yaml"
-	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 )
@@ -69,6 +69,38 @@ func loadConfig() (*Config, error) {
 	return conf, nil
 }
 
+func (in *Instancer) CreateObject(manifest string) (*unstructured.Unstructured, error) {
+	unstructObj, err := ParseK8sYaml(manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	c := discovery.NewDiscoveryClientForConfigOrDie(in.k8sConfig)
+
+	groupResources, err := restmapper.GetAPIGroupResources(c)
+	if err != nil {
+		return nil, err
+	}
+	mapper := restmapper.NewDiscoveryRESTMapper(groupResources)
+	mapping, err := mapper.RESTMapping(unstructObj.GetObjectKind().GroupVersionKind().GroupKind())
+	if err != nil {
+		return nil, err
+	}
+	client, err := dynamic.NewForConfig(in.k8sConfig)
+	if err != nil {
+		return nil, err
+	}
+	resObj, err := client.Resource(mapping.Resource).Create(context.TODO(), unstructObj, metav1.CreateOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return resObj, nil
+}
+
+func SplitYaml(content string) []string {
+	return strings.Split(content, "---")
+}
+
 func (in *Instancer) registerEndpoints() {
 	in.echo.GET("/healthz", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "healthy")
@@ -91,19 +123,17 @@ func (in *Instancer) registerEndpoints() {
 		}
 		// todo: create challenge
 		var err error
-		objs := parseK8sYaml(manifest)
-		for _, obj := range objs {
-			_, err = createObject(
-				in.k8sClientSet,
-				in.k8sConfig,
-				obj)
+		objYamls := SplitYaml(manifest)
+		for _, v := range objYamls {
+			resObj, err := in.CreateObject(v)
 			if err != nil {
 				break
 			}
+			c.Logger().Infof("created %s named: %s", resObj.GetKind(), resObj.GetName())
 		}
 		if err != nil {
 			// todo: handle errors/cleanup incomplete deploys?
-			c.Logger().Errorf("could create an object: %s", err.Error())
+			c.Logger().Errorf("could not create an object: %s", err.Error())
 			return c.JSON(http.StatusInternalServerError, "challenge deploy failed: contact admin")
 		}
 		c.Logger().Info("provisioned new instance")
@@ -111,110 +141,17 @@ func (in *Instancer) registerEndpoints() {
 	})
 
 	in.echo.DELETE("/instances/:id", func(c echo.Context) error {
-		/*_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			return c.JSON(http.StatusInternalServerError, "deletion failed: contact admin")
-		} else {
-			fmt.Printf("Found example-xxxxx pod in default namespace\n")
-		}*/
+
 		return c.JSON(http.StatusAccepted, "destroyed")
 	})
 }
 
 // https://github.com/kubernetes/client-go/issues/193#issuecomment-363318588
-func parseK8sYaml(manifest string) []runtime.Object {
-	sepYamlfiles := strings.Split(manifest, "---")
-	result := make([]runtime.Object, 0, len(sepYamlfiles))
-	for _, f := range sepYamlfiles {
-		if f == "\n" || f == "" {
-			// ignore empty cases
-			continue
-		}
-
-		decode := scheme.Codecs.UniversalDeserializer().Decode
-		obj, _, err := decode([]byte(f), nil, nil)
-		if err != nil {
-			log.Printf("Error while decoding YAML object. Err was: %s", err)
-			continue
-		}
-		result = append(result, obj)
-	}
-	return result
-}
-
-func createObject(clientSet kubernetes.Interface, config *rest.Config, obj runtime.Object) (runtime.Object, error) {
-	// Create a REST mapper that tracks information about the available resources in the cluster.
-	groupResources, err := restmapper.GetAPIGroupResources(clientSet.Discovery())
+func ParseK8sYaml(manifest string) (*unstructured.Unstructured, error) {
+	obj := &unstructured.Unstructured{}
+	err := yaml.UnmarshalStrict([]byte(manifest), obj)
 	if err != nil {
 		return nil, err
 	}
-	rm := restmapper.NewDiscoveryRESTMapper(groupResources)
-
-	// Get some metadata needed to make the REST request.
-	gvk := obj.GetObjectKind().GroupVersionKind()
-	gk := schema.GroupKind{Group: gvk.Group, Kind: gvk.Kind}
-	mapping, err := rm.RESTMapping(gk, gvk.Version)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a client specifically for creating the object.
-	restClient, err := newRestClient(config, mapping.GroupVersionKind.GroupVersion())
-	if err != nil {
-		return nil, err
-	}
-
-	// Use the REST helper to create the object in the "default" namespace.
-	restHelper := resource.NewHelper(restClient, mapping)
-	return restHelper.Create("challenges", false, obj)
+	return obj, nil
 }
-
-func newRestClient(restConfig *rest.Config, gv schema.GroupVersion) (rest.Interface, error) {
-	restConfig.ContentConfig = resource.UnstructuredPlusDefaultContentConfig()
-	restConfig.GroupVersion = &gv
-	if len(gv.Group) == 0 {
-		restConfig.APIPath = "/api"
-	} else {
-		restConfig.APIPath = "/apis"
-	}
-
-	return rest.RESTClientFor(restConfig)
-}
-
-/*
-func watchPods(log echo.Logger) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatalf("error creating configuration: %s", err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("error initializing client: %s", err)
-	}
-
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods("challenges").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		log.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		for _, p := range pods.Items {
-			log.Print(p.ObjectMeta.Name)
-		}
-
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		/*
-
-
-		time.Sleep(20 * time.Second)
-	}
-}
-*/
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "log"
+
+func ExampleParseYaml() {
+	manifest := `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-instance-test-deployment
+  namespace: challenges
+spec:
+  selector:
+  matchLabels:
+    app: nginx-instance-test
+  replicas: 1
+  template:
+    metadata:
+      labels:
+    app: nginx-instance-test
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80
+`
+	obj, err := ParseK8sYaml(manifest)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println(obj.GetObjectKind().GroupVersionKind().Group)
+	log.Println(obj.GetObjectKind().GroupVersionKind().Version)
+	log.Println(obj.GetObjectKind().GroupVersionKind().Kind)
+	// Output:
+}
